014b860c549c71fc019a1c5f64e021c3acc910ec
david cochran <about.trout@gmail.com>
2016-02-08 00:20:03 -0800

    tune CMS for better accuracy

--- processing/src/main/scala/SketchHelpers.scala
+++ processing/src/main/scala/SketchHelpers.scala
@@ -11,16 +11,14 @@ trait SketchHelpers extends RequestHelpers {
 
   val topkCols = SomeColumns("day", "ts", "topkin", "topkout")
 
-  private def getLeaders(cms: TopCMS[String], k: Int = 10) = {
-    cms.heavyHitters
-      .map(x => (x, cms.frequency(x).estimate)).toSeq
-      .sortBy(_._2).reverse.slice(0, k)
-      .toMap
-  }
+  private val eps: Double = 0.0001
+  private val delta: Double = 0.01
+  private val topPct: Double = 0.01
+  private val seed: Int = 369928005
 
   def buildSketches(r: Request) = {
-    val cmsIn = TopPctCMS.monoid[String](0.01, 0.0001, 1, 0.001)
-    val cmsOut = TopPctCMS.monoid[String](0.01, 0.0001, 1, 0.001)
+    val cmsIn = TopPctCMS.monoid[String](eps, delta, seed, topPct)
+    val cmsOut = TopPctCMS.monoid[String](eps, delta, seed, topPct)
 
     (r.ts, cmsIn.create(r.sourceURL), cmsOut.create(r.destURL))
   }
@@ -35,5 +33,12 @@ trait SketchHelpers extends RequestHelpers {
   def prepareTopKRows(ks: TopKQueries) = {
     val (ts, cmsIn, cmsOut) = ks
     (roundDay(ts), ts, getLeaders(cmsIn), getLeaders(cmsOut))
+  }
+
+  private def getLeaders(cms: TopCMS[String], k: Int = 10) = {
+    cms.heavyHitters
+      .map(x => (x, cms.frequency(x).estimate)).toSeq
+      .sortBy(_._2).reverse.slice(0, k)
+      .toMap
   }
 }
